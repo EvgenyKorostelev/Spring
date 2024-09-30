6a497600a169882c74c3789af2cd18c17f4969a5
Evgeny Korostelev <ddc_smoke@mail.ru>
2024-09-30 21:30:05 +0300

    add_dzSem7_securityHell

---
+++ DZSem7/src/test/java/ru/korostelev/DZSem7/DzSem7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.korostelev.DZSem7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DzSem7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
